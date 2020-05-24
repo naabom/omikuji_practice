25cea0712f1936d4de8cfd8527c080d3c3dfa3ce
かな <61941187+naabom@users.noreply.github.com>
2020-05-24 11:54:57 +0900

    initial commit
    練習です。

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click',() => {
+        const results = ['大吉','中吉','小吉','吉','末吉','凶'];
+        btn.textContent = results[Math.floor(Math.random() * results.length)];
+    });
+}
+
